38bdecdbd5261679b6eb274ac93efc8349aa3764
Erick Kusnadi <forgemill@gmail.com>
2015-03-12 14:56:34 -0700

    more refactoring of home screen

--- BoardGameServerAndClient/src/client/HomeScreen.java
+++ BoardGameServerAndClient/src/client/HomeScreen.java
@@ -1,12 +1,6 @@
 package client;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-
 import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.BoxLayout;
 import javax.swing.JList;
 import javax.swing.JButton;
 
@@ -14,10 +8,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Font;
 
-import javax.swing.border.TitledBorder;
 import javax.swing.border.BevelBorder;
-import javax.swing.border.CompoundBorder;
-import javax.swing.border.EtchedBorder;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 import javax.swing.AbstractListModel;
@@ -26,34 +17,27 @@ import javax.swing.SwingConstants;
 import javax.swing.ListSelectionModel;
 
 public class HomeScreen extends JFrame {
-
-	//Uncomment main to test the screen
-	/*
-	public static void main(String args[]){
-		HomeScreen test = new HomeScreen();
-		test.setVisible(true);
-	}
-	*/
 	
-	/**
-	 * Create the frame.
-	 */
-	@SuppressWarnings("unchecked")
+	private JLabel playerLabel;
+	private JList playerList;
+	private JLabel gameLabel;
+	
 	public HomeScreen() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 575, 588);
 		getContentPane().setLayout(null);
 		
 		//Player List
-		JLabel playerLabel = new JLabel("Players");
+		playerLabel = new JLabel("Waiting Room - Players: ");
 		playerLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		playerLabel.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		playerLabel.setBounds(10, 10, 223, 26);
 		getContentPane().add(playerLabel);
 		
-		JList playerList = new JList();
+		playerList = new JList();
 		playerList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		playerList.setFont(new Font("Tahoma", Font.BOLD, 16));
+		/*
 		playerList.setModel(new AbstractListModel() {
 			private static final long serialVersionUID = 1L;
 			String[] values = new String[] {"Carrey", "ninetails", "fireFox", "Rocketmouse", "FireFighter298", "joseph", "Hailey"};
@@ -63,23 +47,23 @@ public class HomeScreen extends JFrame {
 			public Object getElementAt(int index) {
 				return values[index];
 			}
-		});
+		}); */
+		
 		playerList.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
 		playerList.setBounds(10, 47, 223, 491);
 		getContentPane().add(playerList);
 		
 		//Game List
-		JLabel gameLabel = new JLabel("Games");
+		gameLabel = new JLabel("Games");
 		gameLabel.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		gameLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		gameLabel.setBounds(243, 11, 306, 25);
 		getContentPane().add(gameLabel);
+		
 		final JList gameList = new JList();
 		gameList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		
 		/*
-		 * Don't know why it pops up twice when you click on a game
-		 */
 		gameList.setModel(new AbstractListModel() {
 			String[] values = new String[] {"Checkers", "Battleship", "Tic Tac Toe", "Backgammon", "Sorry!", "Monopoly", "Chutes & Ladders"};
 			public int getSize() {
@@ -89,6 +73,8 @@ public class HomeScreen extends JFrame {
 				return values[index];
 			}
 		});
+		*/
+		/*
 		gameList.addListSelectionListener(new ListSelectionListener() {
 		      public void valueChanged(ListSelectionEvent le) {
 		        int idx = gameList.getSelectedIndex();
@@ -98,21 +84,44 @@ public class HomeScreen extends JFrame {
 		          System.out.println("Please choose a language.");
 		      }
 		    });
+		*/
+		
 		gameList.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
 		gameList.setFont(new Font("Tahoma", Font.BOLD, 16));
 		gameList.setBounds(243, 47, 306, 433);
 		getContentPane().add(gameList);
 		
 		//Button for some action
-		JButton btnNewButton = new JButton("Play Game");
-		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 24));
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
+		JButton makeNewRoom = new JButton("Make New Room");
+		makeNewRoom.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		makeNewRoom.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Making new room");
 			}
 		});
-		btnNewButton.setBounds(243, 491, 306, 47);
-		getContentPane().add(btnNewButton);
+		makeNewRoom.setBounds(243, 491, 306, 47);
+		getContentPane().add(makeNewRoom);
 		
 	}
+	
+	public void updatePlayerList(){
+		
+	}
+	
+	public void updateRoomList(){
+		
+	}
+	
+	public void updateView(){
+		
+	}
+	
+	//Uncomment main to test the screen
+	/*
+	public static void main(String args[]){
+		HomeScreen test = new HomeScreen();
+		test.setVisible(true);
+	}
+	*/
 	
 }

8be064732f992c14f72cbed5e49422281681414d
Luke <luke.fiji@gmail.com>
2015-03-11 18:45:58 -0700

    Home Screen Sample
    Home Screen /w Dummy Entries

---
+++ EEL Folder/HomeScreen.java
@@ -0,0 +1,20 @@
+package homescreenapplet;
+
+import javax.swing.JApplet;
+import javax.swing.JSplitPane;
+import java.awt.BorderLayout;
+import java.awt.GridBagLayout;
+import javax.swing.JButton;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+
+public class HomeScreen extends JApplet {
+
+	/**
+	 * Create the applet.
+	 */
+	public HomeScreen() {
+
+	}
+
+}

168f2788817bd6c8f56646e5b73acc68d87e7592
Erick Kusnadi <forgemill@gmail.com>
2015-03-12 10:40:43 -0700

    multi-threading and unique usernames
    based on the multi-client chat server app example

--- BoardGameServerAndClient/src/Client/Client.java
+++ BoardGameServerAndClient/src/Client/Client.java
@@ -1,26 +1,76 @@
 package client;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 
+import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 public class Client {
 
-	public static void main(String[] args) throws IOException{
+	private BufferedReader in;
+	private PrintWriter out;
+	JFrame frame = new JFrame("Chatter");
+	JTextField textField = new JTextField(40);
+	JTextArea messageArea = new JTextArea(8,40);
+	
+	public Client(){
+		textField.setEditable(false);
+		messageArea.setEditable(false);
+		frame.getContentPane().add(textField, "North");
+		frame.getContentPane().add(new JScrollPane(messageArea), "Center");
+		
+		textField.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				out.println(textField.getText());
+				textField.setText("");
+			}
+		});
+	}
+	
+	private String getName() {
+		return JOptionPane.showInputDialog(
+				frame,
+				"Choose a user name",
+				"Screen name selection",
+				JOptionPane.PLAIN_MESSAGE);
+	}
+	
+	private void run() throws IOException {
 		String serverAddress = "localhost";
-        String portNumber = JOptionPane.showInputDialog(
-                "Enter Port Number the Server is running on");
+		String portNumber = JOptionPane.showInputDialog(
+				"Enter Port Number the Server is running on");
         //TODO add some error handling for non valid port numbers
-        Socket s = new Socket(serverAddress, Integer.parseInt(portNumber));
-        BufferedReader input =
-            new BufferedReader(new InputStreamReader(s.getInputStream()));
-        String answer = input.readLine();
-        JOptionPane.showMessageDialog(null, answer);
-        System.exit(0);
-
+        Socket socket = new Socket(serverAddress, Integer.parseInt(portNumber));
+        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        out = new PrintWriter(socket.getOutputStream(), true);
+        
+        while(true) {
+        	String line = in.readLine();
+        	System.out.println(line);
+        	if(line.startsWith("SUBMITNAME")){
+        		out.println(getName());
+        	} else if(line.startsWith("NAMEACCEPTED")) {
+        		textField.setEditable(true);
+        	} else if (line.startsWith("MESSAGE")){
+        		messageArea.append(line.substring(8) + "\n");
+        	}
+        }
+	}
+	
+	public static void main(String[] args) throws IOException{
+		Client client = new Client();
+		client.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		client.frame.setVisible(true);
+		client.run();
 	}
 
 }
--- BoardGameServerAndClient/src/Server/Server.java
+++ BoardGameServerAndClient/src/Server/Server.java
@@ -1,12 +1,18 @@
 package server;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashSet;
 
 public class Server {
 	
+    private static HashSet<String> clientNames = new HashSet<String>();
+    private static HashSet<PrintWriter> writers = new HashSet<PrintWriter>();
+    
 	//Main
 	public static void main(String[] args) throws IOException{
 		
@@ -14,19 +20,65 @@ public class Server {
 		System.out.println("Server is running on Port: " + servSock.getLocalPort());
 		try{
 			while(true){
-				Socket socket = servSock.accept();
-				try {
-					PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-					out.println("meow");
-				} finally {
-					socket.close();
-				}
+				new Handler(servSock.accept()).start();
 			}
-		} finally {
-			servSock.close();
-		}
+		} finally { servSock.close(); }
 		
 	}
 	//End of Main
-
+	
+	private static class Handler extends Thread{
+		private String name;
+		private Socket socket;
+		private BufferedReader in;
+		private PrintWriter out;
+		
+		public Handler(Socket socket){ this.socket = socket; }
+		
+		public void run(){
+			System.out.println("Running new thread");
+			try{
+				in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				out = new PrintWriter(socket.getOutputStream(), true);
+				
+				while(true) {
+					out.println("SUBMITNAME");
+					System.out.println("I'm trying");
+					name = in.readLine();
+					System.out.println("I'm trying I really am");
+					System.out.println(name);
+					if(name == null) { System.out.println("name was null"); return;}
+					synchronized (clientNames) {
+						if(!clientNames.contains(name)) {
+							clientNames.add(name);
+							System.out.println("Added to room: " + name);
+							break;
+						}
+					}
+				}
+				
+				out.println("NAMEACCEPTED");
+				System.out.println("NAMEACCEPTED");
+				writers.add(out);
+				
+				while(true) {
+					String input = in.readLine();
+					if(input == null) { return; }
+					for(PrintWriter writer: writers) {
+						writer.println("MESSAGE" + name + ": " + input);
+					}
+				}
+			} catch (IOException e) {
+				System.err.println(e);
+			} finally {
+				if(name != null) { clientNames.remove(name); }
+				if(out != null) { writers.remove(out); }
+				try {
+					socket.close();
+				} catch (IOException e){ System.err.println("There was an error closing connections, shutting down now"); }
+			}
+		}
+		
+	}
+	
 }

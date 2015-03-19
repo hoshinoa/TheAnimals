baf3705d4fcaa9d10b008ac23e114b092091cd25
Erick Kusnadi <forgemill@gmail.com>
2015-03-18 20:08:02 -0700

    setup checkers board

--- BoardGameServerAndClient/src/games/CheckersGameLogic.java
+++ BoardGameServerAndClient/src/games/CheckersGameLogic.java
@@ -6,11 +6,153 @@ import java.util.HashSet;
 
 public class CheckersGameLogic implements GameLogic{
 
+	public int EMPTY = 0;
+	public int RED = 1;
+	public int BLACK = 2;
+	public int RED_KING = 3;
+	public int BLACK_KING = 4;
+	public int redCount;
+	public int blackCount;
+	
+	private GameState gameState;
+	private Player player1;
+	private Player player2;
+	
 	@Override
 	public void runGame(int boardWidth, int boardHeight,
 			ArrayList<Player> players, HashSet<PrintWriter> writers) {
-		// TODO Auto-generated method stub
 		
+		gameState = new GameState(boardWidth, boardHeight);
+		
+		PrintWriter playerOuts[] = writers.toArray(new PrintWriter[writers.size()]);
+		
+		player1 = players.get(0);
+		player1.setOut(playerOuts[0]);
+		player1.sendMessageToPlayer("MESSAGE" + "Checkers: " + "You are player 1 (W), you will go first");
+		
+		player2 = players.get(1);
+		player2.setOut(playerOuts[1]);
+		player2.sendMessageToPlayer("MESSAGE" + "Checkers: " + "You are player 2 (B), you will go second");
+		
+		player1.sendMessageToPlayer("MESSAGE" + "Checkers: " + "Currently Player 1's turn, please make a move");
+		player2.sendMessageToPlayer("MESSAGE" + "Checkers: " + "Currently Player 1's turn, please wait...");
+		
+		player1.sendMessageToPlayer("MAKEMOVE");
+		player2.sendMessageToPlayer("WAIT");
+		
+		setupCheckers();
+		
+	}
+	
+	public void setupCheckers(){
+		
+		gameState.board[0][1].setPiece("W");
+		gameState.board[0][3].setPiece("W");
+		gameState.board[0][5].setPiece("W");
+		gameState.board[0][7].setPiece("W");
+		gameState.board[0][1].setValue(1);
+		gameState.board[0][3].setValue(1);
+		gameState.board[0][5].setValue(1);
+		gameState.board[0][7].setValue(1);
+		player1.sendMessageToPlayer("PLACEPIECE " + gameState.board[0][1].getPiece() + " " + 1 + " " + 0); 
+		player1.sendMessageToPlayer("PLACEPIECE " + gameState.board[0][3].getPiece() + " " + 3 + " " + 0);
+		player1.sendMessageToPlayer("PLACEPIECE " + gameState.board[0][5].getPiece() + " " + 5 + " " + 0); 
+		player1.sendMessageToPlayer("PLACEPIECE " + gameState.board[0][7].getPiece() + " " + 7 + " " + 0);
+		player2.sendMessageToPlayer("PLACEPIECE " + gameState.board[0][1].getPiece() + " " + 1 + " " + 0); 
+		player2.sendMessageToPlayer("PLACEPIECE " + gameState.board[0][3].getPiece() + " " + 3 + " " + 0);
+		player2.sendMessageToPlayer("PLACEPIECE " + gameState.board[0][5].getPiece() + " " + 5 + " " + 0); 
+		player2.sendMessageToPlayer("PLACEPIECE " + gameState.board[0][7].getPiece() + " " + 7 + " " + 0);
+		
+		gameState.board[1][0].setPiece("W");
+		gameState.board[1][2].setPiece("W");
+		gameState.board[1][4].setPiece("W");
+		gameState.board[1][6].setPiece("W");
+		gameState.board[1][0].setValue(1);
+		gameState.board[1][2].setValue(1);
+		gameState.board[1][4].setValue(1);
+		gameState.board[1][6].setValue(1);
+		player1.sendMessageToPlayer("PLACEPIECE " + gameState.board[1][0].getPiece() + " " + 0 + " " + 1); 
+		player1.sendMessageToPlayer("PLACEPIECE " + gameState.board[1][2].getPiece() + " " + 2 + " " + 1);
+		player1.sendMessageToPlayer("PLACEPIECE " + gameState.board[1][4].getPiece() + " " + 4 + " " + 1); 
+		player1.sendMessageToPlayer("PLACEPIECE " + gameState.board[1][6].getPiece() + " " + 6 + " " + 1);
+		player2.sendMessageToPlayer("PLACEPIECE " + gameState.board[1][0].getPiece() + " " + 0 + " " + 1); 
+		player2.sendMessageToPlayer("PLACEPIECE " + gameState.board[1][2].getPiece() + " " + 2 + " " + 1);
+		player2.sendMessageToPlayer("PLACEPIECE " + gameState.board[1][4].getPiece() + " " + 4 + " " + 1); 
+		player2.sendMessageToPlayer("PLACEPIECE " + gameState.board[1][6].getPiece() + " " + 6 + " " + 1);
+		
+		gameState.board[2][1].setPiece("W");
+		gameState.board[2][3].setPiece("W");
+		gameState.board[2][5].setPiece("W");
+		gameState.board[2][7].setPiece("W");
+		gameState.board[2][1].setValue(1);
+		gameState.board[2][3].setValue(1);
+		gameState.board[2][5].setValue(1);
+		gameState.board[2][7].setValue(1);
+		player1.sendMessageToPlayer("PLACEPIECE " + gameState.board[2][1].getPiece() + " " + 1 + " " + 2); 
+		player1.sendMessageToPlayer("PLACEPIECE " + gameState.board[2][3].getPiece() + " " + 3 + " " + 2);
+		player1.sendMessageToPlayer("PLACEPIECE " + gameState.board[2][5].getPiece() + " " + 5 + " " + 2); 
+		player1.sendMessageToPlayer("PLACEPIECE " + gameState.board[2][7].getPiece() + " " + 7 + " " + 2);
+		player2.sendMessageToPlayer("PLACEPIECE " + gameState.board[2][1].getPiece() + " " + 1 + " " + 2); 
+		player2.sendMessageToPlayer("PLACEPIECE " + gameState.board[2][3].getPiece() + " " + 3 + " " + 2);
+		player2.sendMessageToPlayer("PLACEPIECE " + gameState.board[2][5].getPiece() + " " + 5 + " " + 2); 
+		player2.sendMessageToPlayer("PLACEPIECE " + gameState.board[2][7].getPiece() + " " + 7 + " " + 2);
+		
+		//
+		gameState.board[7][0].setPiece("B");
+		gameState.board[7][2].setPiece("B");
+		gameState.board[7][4].setPiece("B");
+		gameState.board[7][6].setPiece("B");
+		gameState.board[7][0].setValue(2);
+		gameState.board[7][2].setValue(2);
+		gameState.board[7][4].setValue(2);
+		gameState.board[7][6].setValue(2);
+		player1.sendMessageToPlayer("PLACEPIECE " + gameState.board[7][0].getPiece() + " " + 0 + " " + 7); 
+		player1.sendMessageToPlayer("PLACEPIECE " + gameState.board[7][2].getPiece() + " " + 2 + " " + 7);
+		player1.sendMessageToPlayer("PLACEPIECE " + gameState.board[7][4].getPiece() + " " + 4 + " " + 7); 
+		player1.sendMessageToPlayer("PLACEPIECE " + gameState.board[7][6].getPiece() + " " + 6 + " " + 7);
+		player2.sendMessageToPlayer("PLACEPIECE " + gameState.board[7][0].getPiece() + " " + 0 + " " + 7); 
+		player2.sendMessageToPlayer("PLACEPIECE " + gameState.board[7][2].getPiece() + " " + 2 + " " + 7);
+		player2.sendMessageToPlayer("PLACEPIECE " + gameState.board[7][4].getPiece() + " " + 4 + " " + 7); 
+		player2.sendMessageToPlayer("PLACEPIECE " + gameState.board[7][6].getPiece() + " " + 6 + " " + 7);
+		
+		gameState.board[6][1].setPiece("B");
+		gameState.board[6][3].setPiece("B");
+		gameState.board[6][5].setPiece("B");
+		gameState.board[6][7].setPiece("B");
+		gameState.board[6][1].setValue(2);
+		gameState.board[6][3].setValue(2);
+		gameState.board[6][5].setValue(2);
+		gameState.board[6][7].setValue(2);
+		player1.sendMessageToPlayer("PLACEPIECE " + gameState.board[6][1].getPiece() + " " + 1 + " " + 6); 
+		player1.sendMessageToPlayer("PLACEPIECE " + gameState.board[6][3].getPiece() + " " + 3 + " " + 6);
+		player1.sendMessageToPlayer("PLACEPIECE " + gameState.board[6][5].getPiece() + " " + 5 + " " + 6); 
+		player1.sendMessageToPlayer("PLACEPIECE " + gameState.board[6][7].getPiece() + " " + 7 + " " + 6);
+		player2.sendMessageToPlayer("PLACEPIECE " + gameState.board[6][1].getPiece() + " " + 1 + " " + 6); 
+		player2.sendMessageToPlayer("PLACEPIECE " + gameState.board[6][3].getPiece() + " " + 3 + " " + 6);
+		player2.sendMessageToPlayer("PLACEPIECE " + gameState.board[6][5].getPiece() + " " + 5 + " " + 6); 
+		player2.sendMessageToPlayer("PLACEPIECE " + gameState.board[6][7].getPiece() + " " + 7 + " " + 6);
+		
+		gameState.board[5][0].setPiece("B");
+		gameState.board[5][2].setPiece("B");
+		gameState.board[5][4].setPiece("B");
+		gameState.board[5][6].setPiece("B");
+		gameState.board[5][0].setValue(2);
+		gameState.board[2][2].setValue(2);
+		gameState.board[5][4].setValue(2);
+		gameState.board[5][6].setValue(2);
+		player1.sendMessageToPlayer("PLACEPIECE " + gameState.board[5][0].getPiece() + " " + 0 + " " + 5); 
+		player1.sendMessageToPlayer("PLACEPIECE " + gameState.board[5][2].getPiece() + " " + 2 + " " + 5);
+		player1.sendMessageToPlayer("PLACEPIECE " + gameState.board[5][4].getPiece() + " " + 4 + " " + 5); 
+		player1.sendMessageToPlayer("PLACEPIECE " + gameState.board[5][6].getPiece() + " " + 6 + " " + 5);
+		player2.sendMessageToPlayer("PLACEPIECE " + gameState.board[5][0].getPiece() + " " + 0 + " " + 5); 
+		player2.sendMessageToPlayer("PLACEPIECE " + gameState.board[5][2].getPiece() + " " + 2 + " " + 5);
+		player2.sendMessageToPlayer("PLACEPIECE " + gameState.board[5][4].getPiece() + " " + 4 + " " + 5); 
+		player2.sendMessageToPlayer("PLACEPIECE " + gameState.board[5][6].getPiece() + " " + 6 + " " + 5);
+		
+		gameState.mCurrentTurn = 1;
+		
+		blackCount = 12;
+		redCount = 12;
 	}
 
 	@Override
@@ -40,24 +182,8 @@ public class CheckersGameLogic implements GameLogic{
 }
 
 /*
- * import java.util.*;
 
-// You can call setCurrent_turn(int) and getCurrent_turn() after every move.
-// makeMove can be called and there are 4 arguments in this order:
-				//from Row, from Column, to Row, to Column
-// isLegalMove can be called and it has the same arguments
-// makeMove will do the jumps automatically if there are more after the first one.
 
-public class CheckersLogic {
-	public int EMPTY = 0;
-	public int RED = 1;
-	public int BLACK = 2;
-	public int RED_KING = 3;
-	public int BLACK_KING = 4;
-	public int redCount;
-	public int blackCount;
-	public int current_turn;
-	ArrayList<List<Integer>> board;
 	
 	public CheckersLogic(){
 		board = new ArrayList<List<Integer>>();
